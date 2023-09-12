7c80d7eca3b11785db2fbc24d835fa66e86a4861
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2023-09-11 22:19:53 -0500

    New version

--- screenshot2speech.sh
+++ screenshot2speech.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screencapture -s ~/Downloads/screen.png; tesseract ~/Downloads/screen.png ~/Downloads/screen; tts --text "`cat ~/Downloads/screen.txt`" --out_path screen2speech.wav; mplayer screen2speech.wav 
+screencapture -s ~/Downloads/screen.png; tesseract ~/Downloads/screen.png ~/Downloads/screen; say --progress -f ~/Downloads/screen.txt -v "Zoe (Premium)" -o ~/Downloads/screen2speech.aiff; mplayer ~/Downloads/screen2speech.aiff 
